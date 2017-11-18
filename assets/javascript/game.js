0d46c2d79016bbce7bafa4229ae43108dc76154c
boginis1 <hollyzoba@gmail.com>
2017-11-18 16:47:44 -0700

    update new js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,94 @@
- window.onload = function () {
+ 
+ $(document).ready(function () {
 
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
+  var actors = ["Robert Redford", "Brad Pitt", "Kevin Costner", "Paul Newman", "Johnny Depp", "George Clooney", "Ryan Gosling", "Denzel Washington", "Jon Hamm"];
+  var randomIndex = Math.round(Math.random() * (actors.length-1));
+  var word = actors[randomIndex];
+  word = word.replace(/\s/g, " ");
+  console.log(word);
   
-  	var actors = ["Robert Redford", "Brad Pitt", "Kevin Costner", "Paul Newman", "Johnny Depp", "George Clooney", "Ryan Gosling", "Denzel Washington", "Jon Hamm"];
-	var randomIndex = Math.round(Math.random() * (actors.length-1));
-	var word = actors[randomIndex];
-	word = word.replace(/\s/g, "-");
-    console.log(word);
-    
-
-	var guess ;             // Guess
-  	var guesses = [ ];      // Stored guesses
-  	var lives ;             // Lives
-  	var counter ;           // Count correct guesses
-  	var space ;              // Number of spaces in word '-'
+  var guess ;             // Guess
+  var guesses = [];      // Stored guesses
+  var lives ;             // Lives
+  var counter ;           // Count correct guesses
+  var space ;              // Number of spaces in word '-'
+  var result = [];        // results of word
   // Get elements
-  var showLives = document.getElementById("mylives");
+  var showLives = $("#mylives");
+
+ 
+
 
    // create alphabet ul
-  var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-buttons();
+  var createButtons = function () {
     for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
+      var letter = $("<button>"); //creates a button <button></button>
+      letter.addClass("alphabet"); //adding a class alphabet - <button class = "alphabet"></button>
+      letter.text(alphabet[i]); //adding the value of the button - <button class = "alphabet">a</button>
+      $("#buttons").append(letter)//takes out vegas button and puts it into the actual div buttons
     }
   }
-  // Create guesses ul
-   result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
+  createButtons();
+  //handle click buttons
+   $("button.alphabet").click(function(){
+    var clickedLetter = $(this).text()
+    handleClick(clickedLetter)
+  })
 
-    for (var i = 0; i < randomActor.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
-      if (word[i] === "-") {
-        guess.innerHTML = "-";
-        space = 1;
-      } else {
-        guess.innerHTML = "_";
+  var populateResult = function() {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] === " "){
+        result[i] = "*"; //secret message for a space
+      }
+      else {
+        result[i] = "_";
+      }
+    }    
+  }
+
+  populateResult();
+  showResult();
+
+  function showResult(){
+    //create a string from characters in our result array
+    //start with an empty string
+    var finalResult = "";
+    //use each character 
+    for (var i=0; i < result.length; i++) {
+      //add to the total array
+      //remember secret message for space
+      if (result[i] === "*"){
+        finalResult = finalResult + " ";
+      }
+      else {
+        finalResult = finalResult + result[i];
+      }
+      
+    }
+    //show it on the screen as a string
+    $("#currentWord").text(finalResult);
+  }
+
+  // show result after user clicks a button
+  var handleClick = function (letter) {
+    //check if letter is in our word
+    //show it if it is 
+    //discard it if is not
+    for (var i = 0; i < word.length; i++) {
+      if (word[i].toLowerCase()===letter){
+        result[i] = word[i];
       }
 
-      guesses.push(guess);
-      wordHolder.appendChild(correct);
-      correct.appendChild(guess);
     }
+    showResult();
   }
   
   // Show lives
-   comments = function () {
+ comments = function () {
     showLives.innerHTML = "You have " + lives + " lives";
     if (lives < 1) {
       showLives.innerHTML = "Game Over";
@@ -94,10 +123,11 @@ buttons();
   }
    // Reset
 
-  document.getElementById('reset').onclick = function() {
-    correct.parentNode.removeChild(correct);
-    letters.parentNode.removeChild(letters);
-    context.clearRect(0, 0, 400, 400);
-    play();
-  }
-}
+  // document.getElementById('reset').onclick = function() {
+  //   correct.parentNode.removeChild(correct);
+  //   letters.parentNode.removeChild(letters);
+  //   context.clearRect(0, 0, 400, 400);
+  //   play();
+  // }
+});
+ 
