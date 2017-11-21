a613f48ae378e63c69aa2f827de0d21b1ed566a3
boginis1 <hollyzoba@gmail.com>
2017-11-20 19:10:19 -0700

    updated most of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
- 
  $(document).ready(function () {
 
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -6,38 +5,11 @@
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   var actors = ["Robert Redford", "Brad Pitt", "Kevin Costner", "Paul Newman", "Johnny Depp", "George Clooney", "Ryan Gosling", "Denzel Washington", "Jon Hamm"];
-  var randomIndex = Math.round(Math.random() * (actors.length-1));
-  var word = actors[randomIndex];
-  word = word.replace(/\s/g, " ");
-  console.log(word);
-  
-  var guess ;             // Guess
-  var guesses = [];      // Stored guesses
-  var lives ;             // Lives
-  var counter ;           // Count correct guesses
-  var space ;              // Number of spaces in word '-'
+  var word = [];
+  var wrongGuessCounter;           // Count incorrect guesses
+  var maxGuesses = 2;
   var result = [];        // results of word
-  // Get elements
-  var showLives = $("#mylives");
-
- 
-
-
-   // create alphabet ul
-  var createButtons = function () {
-    for (var i = 0; i < alphabet.length; i++) {
-      var letter = $("<button>"); //creates a button <button></button>
-      letter.addClass("alphabet"); //adding a class alphabet - <button class = "alphabet"></button>
-      letter.text(alphabet[i]); //adding the value of the button - <button class = "alphabet">a</button>
-      $("#buttons").append(letter)//takes out vegas button and puts it into the actual div buttons
-    }
-  }
-  createButtons();
-  //handle click buttons
-   $("button.alphabet").click(function(){
-    var clickedLetter = $(this).text()
-    handleClick(clickedLetter)
-  })
+  var wrongGuesses = [];
 
   var populateResult = function() {
     for (var i = 0; i < word.length; i++) {
@@ -49,10 +21,7 @@
       }
     }    
   }
-
-  populateResult();
-  showResult();
-
+  
   function showResult(){
     //create a string from characters in our result array
     //start with an empty string
@@ -73,61 +42,101 @@
     $("#currentWord").text(finalResult);
   }
 
+
+  function startGame(){
+    var randomIndex = Math.round(Math.random() * (actors.length-1));
+    word = actors[randomIndex];
+    word = word.replace(/\s/g, " ");
+    wrongGuessCounter = 0;
+    $("#wrongLetters").text("");
+    wrongGuesses = [];
+    populateResult();
+    showResult();
+    console.log(word);
+  }
+  
+  startGame();
+  
+  
+  
+
+  // Get elements
+  
+
+   // create alphabet buttons
+  var createButtons = function () {
+    for (var i = 0; i < alphabet.length; i++) {
+      var letter = $("<button>"); //creates a button <button></button>
+      letter.addClass("alphabet"); //adding a class alphabet - <button class = "alphabet"></button>
+      letter.text(alphabet[i]); //adding the value of the button - <button class = "alphabet">a</button>
+      $("#buttons").append(letter)//takes out vegas button and puts it into the actual div buttons
+    }
+  }
+  createButtons();
+
+  //handle click buttons
+   $("button.alphabet").click(function(){
+    var clickedLetter = $(this).text()
+    handleClick(clickedLetter)
+  })
+
+ 
+
+  populateResult();
+  showResult();
+  
+
+
+  
+
+  function showWrongGuesses() {
+    var wrongGuessText = "";
+    for (var i = 0; i < wrongGuesses.length; i++) {
+      wrongGuessText += wrongGuesses[i]
+    }
+
+
+    $("#wrongLetters").text(wrongGuessText);
+  }
+
   // show result after user clicks a button
   var handleClick = function (letter) {
     //check if letter is in our word
     //show it if it is 
-    //discard it if is not
+    //put in wrong answer array if it is not
+    var foundIt = false; //flag to let us know wrong guess
     for (var i = 0; i < word.length; i++) {
       if (word[i].toLowerCase()===letter){
         result[i] = word[i];
+        foundIt = true; //found it - don't put the letter in the wrong guess
       }
+      
+     
+    }
+    //check for wrong guess
+    if (foundIt != true){
+      
 
+      if ( wrongGuesses.length === 0 || (wrongGuesses.length != 0 && $.inArray(letter, wrongGuesses)===-1)){
+         wrongGuesses.push(letter);
+         wrongGuessCounter++;
+      }
+      
     }
     showResult();
-  }
-  
-  // Show lives
- comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-      showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === guesses.length) {
-        showLives.innerHTML = "You Win!";
-      }
-    }
-  }
-  // OnClick Function
-   check = function () {
-    list.onclick = function () {
-      var guess = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onclick = null;
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === guess) {
-          guesses[i].innerHTML = guess;
-          counter += 1;
-        } 
-      }
-      var j = (word.indexOf(guess));
-      if (j === -1) {
-        lives -= 1;
-        comments();
-        animate();
-      } else {
-        comments();
-      }
-    }
-  }
-   // Reset
+    showWrongGuesses();
 
-  // document.getElementById('reset').onclick = function() {
-  //   correct.parentNode.removeChild(correct);
-  //   letters.parentNode.removeChild(letters);
-  //   context.clearRect(0, 0, 400, 400);
-  //   play();
-  // }
-});
+    if (wrongGuessCounter === maxGuesses) {
+      showWrongGuesses();
+      alert("YOU LOSE");
+      startGame();
+    }
+  }
+       
+
+    
+
+
  
+  
+});
